a4a9b30961657d251b6e72c87007ba4774e20b8f
IanFeldman <ianrfeldman@gmail.com>
2025-05-01 16:30:23 -0700

    Add basic key scancodes

--- device.h
+++ device.h
@@ -4,6 +4,7 @@
 #include "nkrohid.h"
 
 #define DEBUG_LED PC7
+/* input pins of portc and portd */
 #define PORTC_INPUT ((1 << 2) | (1 << 4) | (1 << 5))
 #define PORTD_INPUT ((1 << 0) | (1 << 1) | (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7))
 
