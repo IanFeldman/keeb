5f574028d860c0355e8843c4683487ae2c1a0408
IanFeldman <ianrfeldman@gmail.com>
2025-05-05 12:04:19 -0700

    Add upper layer

--- device.c
+++ device.c
@@ -63,7 +63,7 @@ uint8_t device_poll(USB_NKRO_Report_Data_t *report)
         report->Keys[SC_TO_IDX(HID_KEYBOARD_SC_A)] |= SC_TO_MSK(HID_KEYBOARD_SC_A) * !(port_b & (1 << 4));
         report->Keys[SC_TO_IDX(HID_KEYBOARD_SC_TAB)] |=
             SC_TO_MSK(HID_KEYBOARD_SC_TAB) * !(port_b & (1 << 5));
-        report->Keys[SC_TO_IDX(HID_KEYBOARD_SC_Z)] |= SC_TO_MSK(HID_KEYBOARD_SC_Z) * !(port_b & (1 << 6));
+        report->Modifier |= HID_KEYBOARD_MODIFIER_LEFTCTRL * !(port_b & (1 << 6));
         report->Modifier |= HID_KEYBOARD_MODIFIER_LEFTSHIFT * !(port_b & (1 << 7));
         /* port c */
         report->Keys[SC_TO_IDX(HID_KEYBOARD_SC_V)] |= SC_TO_MSK(HID_KEYBOARD_SC_V) * !(port_c & (1 << 2));
@@ -79,6 +79,34 @@ uint8_t device_poll(USB_NKRO_Report_Data_t *report)
     }
     else
     {
+        /* port b */
+        report->Keys[SC_TO_IDX(HID_KEYBOARD_SC_Z)] |= SC_TO_MSK(HID_KEYBOARD_SC_Z) * !(port_b & (1 << 0));
+        report->Keys[SC_TO_IDX(HID_KEYBOARD_SC_3_AND_HASHMARK)] |=
+            SC_TO_MSK(HID_KEYBOARD_SC_3_AND_HASHMARK) * !(port_b & (1 << 1));
+        report->Keys[SC_TO_IDX(HID_KEYBOARD_SC_2_AND_AT)] |=
+            SC_TO_MSK(HID_KEYBOARD_SC_2_AND_AT) * !(port_b & (1 << 2));
+        report->Keys[SC_TO_IDX(HID_KEYBOARD_SC_1_AND_EXCLAMATION)] |=
+            SC_TO_MSK(HID_KEYBOARD_SC_1_AND_EXCLAMATION) * !(port_b & (1 << 3));
+        report->Keys[SC_TO_IDX(HID_KEYBOARD_SC_BACKSLASH_AND_PIPE)] |=
+            SC_TO_MSK(HID_KEYBOARD_SC_BACKSLASH_AND_PIPE) * !(port_b & (1 << 4));
+        report->Keys[SC_TO_IDX(HID_KEYBOARD_SC_GRAVE_ACCENT_AND_TILDE)] |=
+            SC_TO_MSK(HID_KEYBOARD_SC_GRAVE_ACCENT_AND_TILDE) * !(port_b & (1 << 5));
+        report->Modifier |= HID_KEYBOARD_MODIFIER_LEFTCTRL * !(port_b & (1 << 6));
+        report->Modifier |= HID_KEYBOARD_MODIFIER_LEFTSHIFT * !(port_b & (1 << 7));
+        /* port c */
+        report->Keys[SC_TO_IDX(HID_KEYBOARD_SC_B)] |= SC_TO_MSK(HID_KEYBOARD_SC_B) * !(port_c & (1 << 2));
+        report->Keys[SC_TO_IDX(HID_KEYBOARD_SC_SPACE)] |=
+            SC_TO_MSK(HID_KEYBOARD_SC_SPACE) * !(port_c & (1 << 5));
+        /* port d */
+        report->Keys[SC_TO_IDX(HID_KEYBOARD_SC_G)] |= SC_TO_MSK(HID_KEYBOARD_SC_G) * !(port_d & (1 << 0));
+        report->Keys[SC_TO_IDX(HID_KEYBOARD_SC_T)] |= SC_TO_MSK(HID_KEYBOARD_SC_T) * !(port_d & (1 << 1));
+        report->Modifier |= HID_KEYBOARD_MODIFIER_LEFTALT * !(port_d & (1 << 4));
+        report->Keys[SC_TO_IDX(HID_KEYBOARD_SC_CLOSING_BRACKET_AND_CLOSING_BRACE)] |=
+            SC_TO_MSK(HID_KEYBOARD_SC_CLOSING_BRACKET_AND_CLOSING_BRACE) * !(port_d & (1 << 5));
+        report->Keys[SC_TO_IDX(HID_KEYBOARD_SC_4_AND_DOLLAR)] |=
+            SC_TO_MSK(HID_KEYBOARD_SC_4_AND_DOLLAR) * !(port_d & (1 << 6));
+        report->Keys[SC_TO_IDX(HID_KEYBOARD_SC_OPENING_BRACKET_AND_OPENING_BRACE)] |=
+            SC_TO_MSK(HID_KEYBOARD_SC_OPENING_BRACKET_AND_OPENING_BRACE) * !(port_d & (1 << 7));
     }
 
     #elif RIGHT
@@ -114,6 +142,34 @@ uint8_t device_poll(USB_NKRO_Report_Data_t *report)
     }
     else
     {
+        /* port b */
+        report->Keys[SC_TO_IDX(HID_KEYBOARD_SC_0_AND_CLOSING_PARENTHESIS)] |=
+            SC_TO_MSK(HID_KEYBOARD_SC_0_AND_CLOSING_PARENTHESIS) * !(port_b & (1 << 0));
+        report->Keys[SC_TO_IDX(HID_KEYBOARD_SC_6_AND_CARET)] |=
+            SC_TO_MSK(HID_KEYBOARD_SC_6_AND_CARET) * !(port_b & (1 << 1));
+        report->Keys[SC_TO_IDX(HID_KEYBOARD_SC_5_AND_PERCENTAGE)] |=
+            SC_TO_MSK(HID_KEYBOARD_SC_5_AND_PERCENTAGE) * !(port_b & (1 << 2));
+        report->Keys[SC_TO_IDX(HID_KEYBOARD_SC_9_AND_OPENING_PARENTHESIS)] |=
+            SC_TO_MSK(HID_KEYBOARD_SC_9_AND_OPENING_PARENTHESIS) * !(port_b & (1 << 3));
+        report->Modifier |= HID_KEYBOARD_MODIFIER_RIGHTALT * !(port_b & (1 << 4));
+        report->Keys[SC_TO_IDX(HID_KEYBOARD_SC_Y)] |= SC_TO_MSK(HID_KEYBOARD_SC_Y) * !(port_b & (1 << 5));
+        report->Keys[SC_TO_IDX(HID_KEYBOARD_SC_H)] |= SC_TO_MSK(HID_KEYBOARD_SC_H) * !(port_b & (1 << 6));
+        report->Keys[SC_TO_IDX(HID_KEYBOARD_SC_N)] |= SC_TO_MSK(HID_KEYBOARD_SC_N) * !(port_b & (1 << 7));
+        /* port c */
+        report->Keys[SC_TO_IDX(HID_KEYBOARD_SC_MINUS_AND_UNDERSCORE)] |=
+            SC_TO_MSK(HID_KEYBOARD_SC_MINUS_AND_UNDERSCORE) * !(port_c & (1 << 2));
+        report->Keys[SC_TO_IDX(HID_KEYBOARD_SC_SPACE)] |= SC_TO_MSK(HID_KEYBOARD_SC_SPACE) * !(port_c & (1 << 4));
+        /* port d */
+        report->Modifier |= HID_KEYBOARD_MODIFIER_RIGHTSHIFT * !(port_d & (1 << 0));
+        report->Keys[SC_TO_IDX(HID_KEYBOARD_SC_EQUAL_AND_PLUS)] |=
+            SC_TO_MSK(HID_KEYBOARD_SC_EQUAL_AND_PLUS) * !(port_d & (1 << 1));
+        report->Keys[SC_TO_IDX(HID_KEYBOARD_SC_8_AND_ASTERISK)] |=
+            SC_TO_MSK(HID_KEYBOARD_SC_8_AND_ASTERISK) * !(port_d & (1 << 4));
+        report->Keys[SC_TO_IDX(HID_KEYBOARD_SC_APOSTROPHE_AND_QUOTE)] |=
+            SC_TO_MSK(HID_KEYBOARD_SC_APOSTROPHE_AND_QUOTE) * !(port_d & (1 << 5));
+        report->Keys[SC_TO_IDX(HID_KEYBOARD_SC_7_AND_AMPERSAND)] |=
+            SC_TO_MSK(HID_KEYBOARD_SC_7_AND_AMPERSAND) * !(port_d & (1 << 6));
+        /* report->Modifier |= HID_KEYBOARD_MODIFIER_RIGHTALT * !(port_d & (1 << 7)); */
     }
 
     #else
